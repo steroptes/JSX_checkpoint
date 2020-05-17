d1d2ada12a206b78ee175dc386901f65c1d74012
steroptes <rjeb.sfar.med@gmail.com>
2020-05-17 14:10:16 +0100

    first commit !

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import einsteinImage from "./einstein-aman.jpg" 
+import videoSample from "./videos/file_example_MP4_480_1_5MG.mp4"
 import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style= {{border:"solid 1px black",maxWidth:"100vw"}}>
+
+        <h1 className="title red"> Einstein is here</h1>
+
+      <div>
+          <img className="images" src={einsteinImage} />
+      </div>
+      
+      <div>
+          <img className="images" src="/einstein.jpg" />
+      </div>
+    </div>
+
+      <video width="320" height="240" controls>
+
+        <source src={videoSample}type="video/mp4"></source>
+ 
+      </video>
     </div>
   );
 }
